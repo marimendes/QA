47a3c6a263432295ed4ec3f420a6381928d68dd7
Mariana Mendes <mare.s.mendes@gmail.com>
2020-08-08 10:58:22 -0300

    Learning about methods on ruby

---
+++ ruby/metodos.rb
@@ -0,0 +1,18 @@
+def diga_ola
+    puts 'Olá!'
+end
+
+def retorna_nome
+    'Mariana'
+end
+
+def soma(v1, v2)
+    total = v1 + v2
+    total
+end
+
+resultado = retorna_nome
+puts resultado
+diga_ola
+res = soma(10,25)
+puts res
