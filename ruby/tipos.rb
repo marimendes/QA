734d4c4742a5083c3174688fff63c7170e879634
Mariana Mendes <mare.s.mendes@gmail.com>
2020-08-08 10:29:44 -0300

    Learning about the types on ruby

---
+++ ruby/tipos.rb
@@ -0,0 +1,17 @@
+# -- Tipos --
+# O tipo de uma variável é definido no momento em que a mesma recebe o valor
+
+produto = 'Macbook air'
+preco = 10.00
+quantidade = 10
+disponivel = true
+
+puts produto.class 
+puts preco.class
+puts quantidade.class 
+puts disponivel.class
+
+puts produto
+puts preco
+puts quantidade 
+puts disponivel
