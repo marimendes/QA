00bbc66d8eb2059f9a451d057abc535d12487a9e
Mariana Mendes <mare.s.mendes@gmail.com>
2020-08-15 15:30:34 -0300

    Implementing the others cases of test on login page

--- capybara/spec/forms_spec.rb
+++ capybara/spec/forms_spec.rb
@@ -8,4 +8,20 @@ describe 'Forms' do
         expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
         expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
+    it 'senha incorreta' do
+        visit "https://training-wheels-protocol.herokuapp.com/login"
+        fill_in 'userId', with: 'stark'
+        fill_in 'password', with: 'jarvis'
+        click_button 'Login'
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash')).to have_content 'Senha é invalida!'
+    end
+    it 'usuário não cadastrado' do
+        visit "https://training-wheels-protocol.herokuapp.com/login"
+        fill_in 'userId', with: 's'
+        fill_in 'password', with: 'jarvis!'
+        click_button 'Login'
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash')).to have_content 'O usuário informado não está cadastrado!'
+    end
 end
