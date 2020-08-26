e0d72ff0e33d06e3874198c82f824999e47067d3
Mariana <mare.s.mendes@gmail.com>
2020-08-26 10:31:11 -0300

    Using regex on hovers test

--- capybara/spec/hovers_spec.rb
+++ capybara/spec/hovers_spec.rb
@@ -5,21 +5,21 @@ describe 'Mouse Hover', :hovers do
     end
 
     it 'quando passar o mouse sobre o blade' do
-        card = find('img[alt=Blade]')
+        card = find('img[alt*=Blade]')
         card.hover
         
         expect(page).to have_content 'Nome: Blade' 
     end
 
     it 'quando passar o mouse sobre o pantera negra' do
-        card = find('img[alt="Pantera Negra"]')
+        card = find('img[alt^=Pantera]')
         card.hover
         
         expect(page).to have_content 'Nome: Pantera Negra' 
     end
 
     it 'quando passar o mouse sobre o homem aranha' do
-        card = find('img[alt="Homem Aranha"]')
+        card = find('img[alt$=Aranha]')
         card.hover
         
         expect(page).to have_content 'Nome: Homem Aranha' 
